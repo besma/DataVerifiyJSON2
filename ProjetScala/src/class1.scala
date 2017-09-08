cc54436d8977de6258aaf62cbf322a5078b6a9eb
besma <khalfi.besma@hotmail.com>
2017-09-08 15:14:56 +0200

    initial files

---
+++ ProjetScala/src/class1.scala
@@ -0,0 +1,11 @@
+
+
+object class1 {
+  def main (args: Array [String]){
+     val message="Bonjour Besma!"
+      println("Hello\"World\n\nbesma" )
+      var x: Int =14;
+      var y: Double = 2.0;
+      var z =12;
+  }
+}
